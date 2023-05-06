e814dfb739c9e6fee06937bed31954272798b77a
rg3915 <regis42santos@gmail.com>
2023-05-06 06:47:11 -0300

    Add others pages.

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -8,4 +8,10 @@ app_name = 'core'
 urlpatterns = [
     path('', v.index, name='index'),  # noqa E501
     path('dashboard/', v.dashboard, name='dashboard'),  # noqa E501
+    path('table/', v.table, name='table'),  # noqa E501
+    path('billing/', v.billing, name='billing'),  # noqa E501
+    path('notification/', v.notification, name='notification'),  # noqa E501
+    path('profile/', v.profile, name='profile'),  # noqa E501
+    path('signin/', v.signin, name='signin'),  # noqa E501
+    path('signup/', v.signup, name='signup'),  # noqa E501
 ]
--- backend/core/views.py
+++ backend/core/views.py
@@ -10,3 +10,33 @@ def index(request):
 def dashboard(request):
     template_name = 'dashboard.html'
     return render(request, template_name)
+
+
+def table(request):
+    template_name = 'tables.html'
+    return render(request, template_name)
+
+
+def billing(request):
+    template_name = 'billing.html'
+    return render(request, template_name)
+
+
+def notification(request):
+    template_name = 'notifications.html'
+    return render(request, template_name)
+
+
+def profile(request):
+    template_name = 'profile.html'
+    return render(request, template_name)
+
+
+def signin(request):
+    template_name = 'sign-in.html'
+    return render(request, template_name)
+
+
+def signup(request):
+    template_name = 'sign-up.html'
+    return render(request, template_name)
