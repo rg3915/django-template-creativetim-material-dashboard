6e659459dcebf1e54f80a71aa6742f24933ea7d6
rg3915 <regis42santos@gmail.com>
2023-05-06 05:05:54 -0300

    Add base project.

---
+++ backend/core/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from backend.core import views as v
+
+app_name = 'core'
+
+
+urlpatterns = [
+    path('', v.index, name='index'),  # noqa E501
+    # path('dashboard/', v.dashboard, name='dashboard'),  # noqa E501
+]
